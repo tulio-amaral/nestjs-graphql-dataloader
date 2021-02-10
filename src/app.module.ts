4b76b1a91ab68b1302ea4bb319dfa0367c40a4ad
tulio-amaral <tuliosantos@gee.inatel.br>
2021-02-10 11:19:00 -0300

    feat: add author.resolver to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,15 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import RepoModule from './repo.module';
+import AuthorResolver from './resolvers/author.resolver';
+
+const GraphQLImport = [AuthorResolver];
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
     RepoModule,
+    ...GraphQLImport,
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
       playground: true,
