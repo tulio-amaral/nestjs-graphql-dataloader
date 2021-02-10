854739170684e0ce4eb8e63ed8bb43c3b5052db5
tulio-amaral <tuliosantos@gee.inatel.br>
2021-02-10 11:00:20 -0300

    feat: change on app.module to integrate GraphQL modules at the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import RepoModule from './repo.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), RepoModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    RepoModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      playground: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
